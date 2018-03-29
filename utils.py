2e6eebce2e47ced7508aeb49c0a77dbe22c6e2fb
JifuZhao <jifuzhao@gmail.com>
2018-03-29 14:16:55 -0500

    fix triplet bugs in triplet_net

--- utils.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-__author__  =   "Jifu Zhao"
-__email__   =   "jifuzhao@gmail.com"
-__date__    =   "03/20/2018"
-__update__  =   "03/20/2018"
-"""
-
